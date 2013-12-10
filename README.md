1e4aa71b1abc660d6d3addefb16b0d53b3e3ab2c
Cosmo Mao <gongyuan.mao@cosmocommerce.com>
2013-12-10 18:48:12 +0800

    module initial

---
+++ README.md
@@ -0,0 +1,52 @@
+#Magento Alipay Payment Gateway Module![project status](http://stillmaintained.com/sidealice/aliyun-paas.png)#
+==============
+
+Support payment types:
+* Create Forex Trade:Internation Payments Type
+* Trade Create By Buyer
+* Create Direct Pay By User
+* Create Partner Trade By Buyer
+
+If you have trouble to choose the type please contact payment@cosmocommerce.com 
+
+##Issues##
+
+Before change the order status need to compare the status flow.
+
+##Change Log##
+
+##Requirements##
+
+This module has the following requirements:
+
+ - PHP 5.3+
+
+Dependency:
+
+ - Magneto 1.x
+
+##Installation##
+
+1 Install modman:
+
+bash < <(wget -O - https://raw.github.com/colinmollenhour/modman/master/modman-installer)
+
+or
+
+bash < <(curl -s https://raw.github.com/colinmollenhour/modman/master/modman-installer)
+
+2 modman init 
+
+3 modman clone https://github.com/cosmocommerce/magento-updater.git
+
+4 modman clone https://github.com/cosmocommerce/magento-alipay.git
+
+
+## Documentation ##
+
+ - 
+ 
+## Maintainer ##
+
+ - http://www.cosmocommerce.com
+ - opensource@cosmocommerce.com
---
+++ code/Model/Payment.php
@@ -0,0 +1,344 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * @category    CosmoCommerce
+ * @package     CosmoCommerce_Sinapay
+ * @copyright   Copyright (c) 2009-2013 CosmoCommerce,LLC. (http://www.cosmocommerce.com)
+ * @contact :
+ * T: +86-021-66346672
+ * L: Shanghai,China
+ * M:sales@cosmocommerce.com
+ */
+class CosmoCommerce_Sinapay_Model_Payment extends Mage_Payment_Model_Method_Abstract
+{
+    protected $_code  = 'sinapay_payment';
+    protected $_formBlockType = 'sinapay/form';
+    
+    //测试URL: http://testpay.sina.com.cn/openserver/1/order/receiver
+    //线上URL: http://pay.sina.com.cn/openserver/1/order/receiver
+    //测试URL: http://testpay.sina.com.cn/openserver/1/order/query
+    //线上URL: http://pay.sina.com.cn/openserver/1/order/query
+    
+	protected $_gateway="http://testpay.sina.com.cn/openserver/1/order/?";
+
+    
+    // Sinapay return codes of payment
+    const RETURN_CODE_ACCEPTED      = 'Success';
+    const RETURN_CODE_TEST_ACCEPTED = 'Success';
+    const RETURN_CODE_ERROR         = 'Fail';
+
+    // Payment configuration
+    protected $_isGateway               = false;
+    protected $_canAuthorize            = true;
+    protected $_canCapture              = true;
+    protected $_canCapturePartial       = false;
+    protected $_canRefund               = false;
+    protected $_canVoid                 = false;
+    protected $_canUseInternal          = false;
+    protected $_canUseCheckout          = true;
+    protected $_canUseForMultishipping  = false;
+
+    // Order instance
+    protected $_order = null;
+
+    /**
+     *  Returns Target URL
+     *
+     *  @return	  string Target URL
+     */
+    public function getSinapayUrl()
+    {
+        $url = $this->_gateway;
+        return $url;
+    }
+
+    /**
+     *  Return back URL
+     *
+     *  @return	  string URL
+     */
+	protected function getReturnURL()
+	{
+		return Mage::getUrl('sinapay/payment/notify/', array('_secure' => true));
+	}
+
+	/**
+	 *  Return URL for Sinapay success response
+	 *
+	 *  @return	  string URL
+	 */
+	protected function getSuccessURL()
+	{
+		return Mage::getUrl('checkout/onepage/success', array('_secure' => true));
+	}
+
+    /**
+     *  Return URL for Sinapay failure response
+     *
+     *  @return	  string URL
+     */
+    protected function getErrorURL()
+    {
+        return Mage::getUrl('sinapay/payment/error', array('_secure' => true));
+    }
+
+	/**
+	 *  Return URL for Sinapay notify response
+	 *
+	 *  @return	  string URL
+	 */
+	protected function getNotifyURL()
+	{
+		return Mage::getUrl('sinapay/payment/notify/', array('_secure' => true));
+	}
+
+    /**
+     * Capture payment
+     *
+     * @param   Varien_Object $orderPayment
+     * @return  Mage_Payment_Model_Abstract
+     */
+    public function capture(Varien_Object $payment, $amount)
+    {
+        $payment->setStatus(self::STATUS_APPROVED)
+            ->setLastTransId($this->getTransactionId());
+
+        return $this;
+    }
+
+    /**
+     *  Form block description
+     *
+     *  @return	 object
+     */
+    public function createFormBlock($name)
+    {
+        $block = $this->getLayout()->createBlock('sinapay/form_payment', $name);
+        $block->setMethod($this->_code);
+        $block->setPayment($this->getPayment());
+
+        return $block;
+    }
+
+    /**
+     *  Return Order Place Redirect URL
+     *
+     *  @return	  string Order Redirect URL
+     */
+    public function getOrderPlaceRedirectUrl()
+    {
+        return Mage::getUrl('sinapay/payment/pay');
+    }
+
+    /**
+     *  Return Standard Checkout Form Fields for request to Sinapay
+     *
+     *  @return	  array Array of hidden form fields
+     */
+    public function getStandardCheckoutFormFields()
+    {
+        $session = Mage::getSingleton('checkout/session');
+        
+        $order = $this->getOrder();
+        if (!($order instanceof Mage_Sales_Model_Order)) {
+            Mage::throwException($this->_getHelper()->__('Cannot retrieve order object'));
+        }
+		
+		
+		$converted_final_price=$order->getGrandTotal();
+		$app_id=$this->getConfigData('app_id');
+		$cuid=$this->getConfigData('partner_id');
+		$appkey=$this->getConfigData('security_code');
+		$order_id= $app_id."_".$order->getRealOrderId();
+     
+        order_time  20071117020101
+        amount  1000
+        notify_url
+        bank_code   可空
+        product_name    可空
+        product_num     可空
+        product_id      可空
+        product_desc    可空
+        redo_flag       0
+        sign_msg        strtolower(md5('cuid={cuid}&appkey={appkey}&order_id={order_id}&order_time ={order_time}&amount={amount}&notify_url={notify_url}&bank_code={bank_code}&product_name={product_name}&product_num={product_num}&product_id={product_id}&product_desc={product_desc}&redo_flag={redo_flag}&key ={app_secret}'));
+     
+        $parameter = array('cuid'           => $cuid,
+                           'appkey'           => $appkey,
+                           'order_id'      =>$order_id, // order ID
+                           'return_url'        => $this->getReturnURL(),
+                           'notify_url'        => $this->getNotifyURL(),
+                           '_input_charset'    => 'utf-8',
+                           'subject'           => $order->getRealOrderId(), 
+                           'body'              => $order->getRealOrderId(),
+                           'total_fee'             => sprintf('%.2f', $converted_final_price) ,
+                           'currency'      => 'USD'
+                        );
+		 
+		
+		$parameter = $this->para_filter($parameter);
+		$security_code = $this->getConfigData('security_code');
+		$sign_type = 'MD5';
+		
+		$sort_array = array();
+		$arg = "";
+		$sort_array = $this->arg_sort($parameter); //$parameter
+		
+		while (list ($key, $val) = each ($sort_array)) {
+			$arg.=$key."=".$this->charset_encode($val,$parameter['_input_charset'])."&";
+		}
+		
+		$prestr = substr($arg,0,count($arg)-2);
+		
+		$mysign = $this->sign($prestr.$security_code);
+		
+		$fields = array();
+		$sort_array = array();
+		$arg = "";
+		$sort_array = $this->arg_sort($parameter); //$parameter
+		while (list ($key, $val) = each ($sort_array)) {
+			$fields[$key] = $this->charset_encode($val,'utf-8');
+		}
+		$fields['sign'] = $mysign;
+		$fields['sign_type'] = $sign_type;
+        return $fields;
+    }
+    
+	public function sign($prestr) {
+		$mysign = md5($prestr);
+		return $mysign;
+	}
+    
+	public function para_filter($parameter) {
+		$para = array();
+		while (list ($key, $val) = each ($parameter)) {
+			if($key == "sign" || $key == "sign_type" || $val == "")continue;
+			else	$para[$key] = $parameter[$key];
+
+		}
+		return $para;
+	}
+	
+	public function arg_sort($array) {
+		ksort($array);
+		reset($array);
+		return $array;
+	}
+
+	public function charset_encode($input,$_output_charset ,$_input_charset ="GBK" ) {
+		$output = "";
+		if($_input_charset == $_output_charset || $input ==null) {
+			$output = $input;
+		} elseif (function_exists("mb_convert_encoding")){
+			$output = mb_convert_encoding($input,$_output_charset,$_input_charset);
+		} elseif(function_exists("iconv")) {
+			$output = iconv($_input_charset,$_output_charset,$input);
+		} else die("sorry, you have no libs support for charset change.");
+		return $output;
+	}
+   
+	/**
+	 * Return authorized languages by Sinapay
+	 *
+	 * @param	none
+	 * @return	array
+	 */
+	protected function _getAuthorizedLanguages()
+	{
+		$languages = array();
+		
+        foreach (Mage::getConfig()->getNode('global/payment/sinapay_payment/languages')->asArray() as $data) 
+		{
+			$languages[$data['code']] = $data['name'];
+		}
+		
+		return $languages;
+	}
+	
+	/**
+	 * Return language code to send to Sinapay
+	 *
+	 * @param	none
+	 * @return	String
+	 */
+	protected function _getLanguageCode()
+	{
+		// Store language
+		$language = strtoupper(substr(Mage::getStoreConfig('general/locale/code'), 0, 2));
+
+		// Authorized Languages
+		$authorized_languages = $this->_getAuthorizedLanguages();
+
+		if (count($authorized_languages) === 1) 
+		{
+			$codes = array_keys($authorized_languages);
+			return $codes[0];
+		}
+		
+		if (array_key_exists($language, $authorized_languages)) 
+		{
+			return $language;
+		}
+		
+		// By default we use language selected in store admin
+		return $this->getConfigData('language');
+	}
+
+
+
+    /**
+     *  Output failure response and stop the script
+     *
+     *  @param    none
+     *  @return	  void
+     */
+    public function generateErrorResponse()
+    {
+        die($this->getErrorResponse());
+    }
+
+    /**
+     *  Return response for Sinapay success payment
+     *
+     *  @param    none
+     *  @return	  string Success response string
+     */
+    public function getSuccessResponse()
+    {
+        $response = array(
+            'Pragma: no-cache',
+            'Content-type : text/plain',
+            'Version: 1',
+            'OK'
+        );
+        return implode("\n", $response) . "\n";
+    }
+
+    /**
+     *  Return response for Sinapay failure payment
+     *
+     *  @param    none
+     *  @return	  string Failure response string
+     */
+    public function getErrorResponse()
+    {
+        $response = array(
+            'Pragma: no-cache',
+            'Content-type : text/plain',
+            'Version: 1',
+            'Document falsifie'
+        );
+        return implode("\n", $response) . "\n";
+    }
+
+}
